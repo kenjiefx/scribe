3748d3c6af5b604327bd3a13e073e92f6e2070c3
kenjiefx <terradoflex@gmail.com>
2025-11-30 09:12:43 +0800

    initial application layer

---
+++ src/Application/UseCases/PullUseCase.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Kenjiefx\Scribe\Application\UseCases;
+
+use Kenjiefx\Scribe\Core\Extractors\ExtractService;
+use Kenjiefx\Scribe\Core\Pullers\PullService;
+use Kenjiefx\Scribe\Core\Pulls\PullMapper;
+use Kenjiefx\Scribe\Core\Pulls\PullModel;
+use Kenjiefx\Scribe\Core\Sources\SourceIterator;
+use Kenjiefx\Scribe\Core\Sources\SourceModel;
+use Kenjiefx\Scribe\Interfaces\FileManagerInterface;
+
+class PullUseCase
+{
+    public function __construct(
+        private PullService $pullService,
+        private PullMapper $pullMapper,
+        private FileManagerInterface $fileManager
+    ) {
+
+    }
+
+    public function execute(SourceIterator $sources): void
+    {
+        $pullMapper = $this->pullMapper;
+        $this->pullService->pull($sources, function (SourceModel $source, string $file) use ($sources, $pullMapper) {
+            $pullMapper->map($source, $file, function (PullModel $pull, string $content) {
+                $destinationPath = $pull->getFullToPath();
+                $this->makeDirIfNotExists(dirname($destinationPath));
+                $this->fileManager->createFile($destinationPath, $content);
+            });
+        });
+    }
+
+    public function makeDirIfNotExists(string $path): void
+    {
+        if (!is_dir($path)) {
+            mkdir($path, 0777, true);
+        }
+    }
+}
